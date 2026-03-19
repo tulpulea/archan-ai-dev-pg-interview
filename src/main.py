5fa86380a514bc5a599c8d102bb4996d0c60a60d
Archan Tulpule <tulpulea@msu.edu>
2026-03-19 14:56:03 -0400

    add basic LLM product extraction logic to main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from google import genai
 
 #loads gemini api key as environment variable
 load_dotenv() 
@@ -6,4 +7,26 @@ load_dotenv()
 with open("inputs/sample_promotion.txt", "r",encoding="utf-8") as f:
     promotion_text = f.read()
 
-print(promotion_text)
+client = genai.Client()
+
+response = client.models.generate_content(
+    model = "gemini-3-flash-preview",
+    contents=f"""
+Read the following product promotion text and extract:
+- product name
+- brand
+- percentage discount
+- promotion start date
+- promotion end date
+- list of eligible retailers
+
+Return ONLY a valid JSON object with these fields.
+Do not include any extra text.
+
+Product promotion:
+{promotion_text}
+"""
+)
+
+print(response.text)
+
