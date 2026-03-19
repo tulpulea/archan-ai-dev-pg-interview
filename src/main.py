9f745802d8d91c3d934a4edcef45d7e03f2471c3
Archan Tulpule <tulpulea@msu.edu>
2026-03-19 15:28:48 -0400

    edit main to construct initial prompt and check prompt creation

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from google import genai
+from prompts import create_initial_field_extract_prompt
 
 #loads gemini api key as environment variable
 load_dotenv() 
@@ -8,25 +9,8 @@ with open("inputs/sample_promotion.txt", "r",encoding="utf-8") as f:
     promotion_text = f.read()
 
 client = genai.Client()
+model = "gemini-3-flash-preview"
 
-response = client.models.generate_content(
-    model = "gemini-3-flash-preview",
-    contents=f"""
-Read the following product promotion text and extract:
-- product name
-- brand
-- percentage discount
-- promotion start date
-- promotion end date
-- list of eligible retailers
+initial_prompt = create_initial_field_extract_prompt(promotion_text)
 
-Return ONLY a valid JSON object with these fields.
-Do not include any extra text.
-
-Product promotion:
-{promotion_text}
-"""
-)
-
-print(response.text)
-
+print(initial_prompt)
