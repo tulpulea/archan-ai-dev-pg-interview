6853ad9c1a4831fafefe2d8125545e3bd0ea9419
Archan Tulpule <tulpulea@msu.edu>
2026-03-19 14:34:37 -0400

    add input file reading

--- src/main.py
+++ src/main.py
@@ -2,3 +2,8 @@ from dotenv import load_dotenv
 
 #loads gemini api key as environment variable
 load_dotenv() 
+
+with open("inputs/sample_promotion.txt", "r",encoding="utf-8") as f:
+    promotion_text = f.read()
+
+print(promotion_text)
